08a679f05c4f64fe7462e43f6f1f04d170ed90e2
AlvesKZ <igoralvex01@gmail.com>
2025-07-03 20:43:49 -0300

    Cria pagina de login de usuário

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Aluno(e) {
+export default function Aluno() {
   return (
     <Container>
       <h1>Aluno</h1>
--- src/pages/Fotos/index.js
+++ src/pages/Fotos/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Fotos(e) {
+export default function Fotos() {
   return (
     <Container>
       <h1>Fotos</h1>
